62871f03ba91e706dc898429301a1b3b2bf238cf
gitRajatKumar <edu.kumarajat@gmail.com>
2025-03-19 10:57:16 +0530

    register cap

--- backend/app.js
+++ backend/app.js
@@ -3,15 +3,17 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser');
 const connectToDB = require('./db/db');
 const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
 
 connectToDB();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 
 
 app.get('/', (req, res) => {
@@ -19,6 +21,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
 
 
 module.exports = app;
